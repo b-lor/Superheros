7ba1ac54f0d5d547dbf3a224224c298bb427a3bf
b-lor <blorwi@gmail.com>
2018-11-29 12:01:33 -0600

    Fixed Server Error  when running program

--- Superheros/Controllers/HeroController.cs
+++ Superheros/Controllers/HeroController.cs
@@ -11,6 +11,32 @@ namespace Superheros.Controllers
         // GET: Hero
         public ActionResult Index()
         {
+            ViewBag.Message = "Index page.";
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Message = "Create page.";
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            ViewBag.Message = "Delete page.";
+
+            return View();
+        }
+
+        public ActionResult Details()
+        {
+            ViewBag.Message = "Details page.";
+
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            ViewBag.Message = "Edit page.";
+
             return View();
         }
     }
